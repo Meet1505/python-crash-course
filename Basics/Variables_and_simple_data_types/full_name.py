ee7ac6cbf305cadc33c669bc1da23bf16a78bc14
Meet Fariya <meet.fariya97@gmail.com>
2026-04-11 17:31:15 +0530

    f strings commit 1

---
+++ Basics/Variables_and_simple_data_types/full_name.py
@@ -0,0 +1,15 @@
+first_name = "vyomi"
+last_name = "fariya"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "vyomi"
+last_name = "fariya"
+full_name = f"{first_name} {last_name}"
+print(f"Hi, {first_name.title()} {last_name.title()}!")
+
+first_name = "vyomi"
+last_name = "fariya"
+full_name = f"{first_name} {last_name}"
+message = f"Hi, {first_name.title()} {last_name.title()}!"
+print(message)
