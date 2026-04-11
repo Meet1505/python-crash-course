995a3822b1e513f2c470e5c9a0a568dadd6de3f2
Meet Fariya <meet.fariya97@gmail.com>
2026-04-11 17:41:34 +0530

    strip commit

--- Basics/Variables_and_simple_data_types/apostrophe.py
+++ Basics/Variables_and_simple_data_types/apostrophe.py
@@ -1,6 +1,10 @@
 message = "Hi, I am world's best human"
 print(message)
 
-message = 'Hi, I am world's best human'
+message = "Hi, I am world's best human"
 print(message)
 
+message = " ronaldo "
+print(message.rstrip())
+print(message.lstrip())
+print(message.strip())
